97c5c4c0bef2745ce36e755ff0ddc94d7d555807
arendu <adithya.r@gmail.com>
2014-03-01 20:02:04 -0500

    translate pipeline now uses inverse segmentation

--- translate.py
+++ translate.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
     os.system('python linearChain.py -fst lc.fst -str ' + s.strip() + ' -sym data/symf.bin')
     os.system('fstcompose lc.fst data/seg.fst > lc.seg.fst')
     os.system('fstcompose lc.seg.fst data/trans.fst > lc.trans.fst')
-    os.system('python outputPaths.py lc.trans.fst ' + str(n))
-    os.system('rm lc.fst lc.trans.fst lc.seg.fst')
+    os.system('fstcompose lc.trans.fst data/inv_seg.fst > lc.out.fst')
+    os.system('python outputPaths.py lc.out.fst ' + str(n))
+    os.system('rm lc.fst lc.trans.fst lc.seg.fst lc.out.fst')
