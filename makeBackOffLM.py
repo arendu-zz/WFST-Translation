73b16bc6aa57d4a3e6cb04fcef6ee07a80d030f1
arendu <adithya.r@gmail.com>
2014-03-04 19:11:10 -0500

    print states added to lm with backoff

--- makeBackOffLM.py
+++ makeBackOffLM.py
@@ -6,13 +6,17 @@ NULL = '_null_'
 
 
 def trysplit(gramline):
-    print gramline
     try:
         [p, ng, b] = gramline.split('\t')
         return -float(p), ng, -float(b)
     except:
         [p, ng] = gramline.split('\t')
         return -float(p), ng, None
+
+
+def add_arc_pr(lmfst, fid, tid, isy, osy, wt):
+    lmfst.add_arc(fid, tid, isy, osy, wt)
+    print 'added arc', fid, tid
 
 
 if __name__ == '__main__':
@@ -33,9 +37,10 @@ if __name__ == '__main__':
         if uni_line.strip() != '' and len(uni_line.split('\t')) > 1:
             [p, ng, bk] = trysplit(uni_line)
             lm_id[ng] = lm_id.get(ng, len(lm_id))
-            lm_fst.add_arc(lm_id[NULL], u, ng, ng, p)
+            add_arc_pr(lm_fst,lm_id[NULL], u, ng, ng, p)
+
             if bk is not None:
-                lm_fst.add_arc(u, lm_id[NULL], fst.EPSILON, fst.EPSILON, bk)
+                add_arc_pr(lm_fst,u, lm_id[NULL], fst.EPSILON, fst.EPSILON, bk)
             if ng == '</s>':
                 lm_fst[lm_id[ng]].final = True
             u += 1
@@ -48,9 +53,9 @@ if __name__ == '__main__':
             lm_id[ng1, ng2] = lm_id.get((ng1, ng2), len(lm_id))
             from_id = lm_id[ng1]
             to_id = lm_id[ng1, ng2]
-            lm_fst.add_arc(from_id, to_id, ng2, ng2, p)
+            add_arc_pr(lm_fst,from_id, to_id, ng2, ng2, p)
             if bk is not None:
-                lm_fst.add_arc(to_id, lm_id[ng2], fst.EPSILON, fst.EPSILON, bk)
+                add_arc_pr(lm_fst,to_id, lm_id[ng2], fst.EPSILON, fst.EPSILON, bk)
             if ng2 == '</s>':
                 lm_fst[lm_id[ng1, ng2]].final = True
             b += 1
@@ -62,14 +67,14 @@ if __name__ == '__main__':
             lm_id[ng2, ng3] = lm_id.get((ng2, ng3), len(lm_id))
             from_id = lm_id[ng1, ng2]
             to_id = lm_id[ng2, ng3]
-            lm_fst.add_arc(from_id, to_id, ng3, ng3, p)
+            add_arc_pr(lm_fst,from_id, to_id, ng3, ng3, p)
             if ng3 == '</s>':
                 lm_fst[lm_id[ng2, ng3]].final = True
 
-    #pprint.pprint(lm_id)
+    pprint.pprint(lm_id)
     #connect init to start
-    print lm_id['<s>']
-    lm_fst.add_arc(lm_id[INITIAL], lm_id[NULL], '<s>', '<s>', 99.0)
+
+    add_arc_pr(lm_fst,lm_id[INITIAL], lm_id[NULL], fst.EPSILON, fst.EPSILON, 0.0)
     lm_fst.write('data/explm.fst', sym_e, sym_e)
     """
     99 <s> -1.640621
