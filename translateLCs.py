fe137271a145e2677b0c632f00f36d029db39922
arendu <adithya.r@gmail.com>
2014-03-05 05:54:30 -0500

    translate all lcs

--- translateLCs.py
+++ translateLCs.py
@@ -11,10 +11,6 @@ if __name__ == '__main__':
     dir = "t-" + str(prn)
     for idx, s in enumerate(open(input_file, 'r').readlines()):
         print idx, s
-        if idx in [20, 22, 27]:  # the 3 files that had mem issues
-            pass
-        else:
-            continue
         os.system('mkdir %s' % dir)
         os.system('cp lcs/lc' + str(idx) + ('.fst %s' % dir) + '/lc.fst ')
         os.system('fstcompose %s' % dir + '/lc.fst data/seg.fst > %s' % dir + '/lc.seg.fst')
