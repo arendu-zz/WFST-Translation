9f2636e5d5230bcf4dc7d039d9d25ebc84160f19
arendu <adithya.r@gmail.com>
2014-03-04 21:52:56 -0500

    things seem to work now

--- translate.py
+++ translate.py
@@ -1,16 +1,17 @@
 import os
 import sys
 import pdb
+
 if __name__ == '__main__':
     n = 1  # int(sys.argv[1])
     reordering = False
     input_file = str(sys.argv[1])
-    for s in open(input_file,'r').readlines():
+    for s in open(input_file, 'r').readlines():
         #print s.strip()
-        s = [str('"'+i+'"') for i in s.split()]
+        s = [str('"' + i + '"') for i in s.split()]
         #print ' '.join(s)
         os.system('mkdir -p t2')
-        os.system('python linearChain.py -fst t2/lc.fst -str ' +' '.join(s) + ' -sym data/symf.bin')
+        os.system('python linearChain.py -fst t2/lc.fst -str ' + ' '.join(s) + ' -sym data/symf.bin')
         os.system('fstcompose t2/lc.fst data/seg.fst > t2/lc.seg.fst')
 
         if reordering:
@@ -24,10 +25,10 @@ if __name__ == '__main__':
         os.system('fstconcat data/__s__.fst t2/lc.out.fst > t2/lc.s.out.fst')
         os.system('fstconcat t2/lc.s.out.fst data/_s_.fst > t2/lc.s.s.out.fst')
         os.system('fstarcsort --sort_type="olabel" t2/lc.s.s.out.fst > t2/lc.s.s.out.sorted.fst')
-        os.system('fstcompose t2/lc.s.s.out.sorted.fst data/unk.fst > t2/lc.unk.fst')
-
-        #os.system('fstcompose t2/lc.unk.fst data/explm-new.fst > t2/lc.final.fst')
-        os.system('fstcompose t2/lc.s.s.out.sorted.fst data/explm-new.fst > t2/lc.final.fst')
+        os.system('fstcompose t2/lc.s.s.out.sorted.fst data/unk.fst > t2/lc.unk.unsorted.fst')
+        os.system('fstarcsort --sort_type="olabel" t2/lc.unk.unsorted.fst > t2/lc.unk.fst')
+        os.system('fstcompose t2/lc.unk.fst data/explm-new.fst > t2/lc.final.fst')
+        #os.system('fstcompose t2/lc.s.s.out.sorted.fst data/explm-new.fst > t2/lc.final.fst')
         #os.system('python outputPaths.py t2/lc.final.fst ' + str(n))
         os.system('python printFinal.py t2/lc.final.fst')
         #os.system('rm -rf t2')
