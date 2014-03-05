f3f893c5ef498447143da3561ba58b6d27657b66
arendu <adithya.r@gmail.com>
2014-03-05 13:25:20 -0500

    re made the phrase swapping file added it to train pipleline

--- train.py
+++ train.py
@@ -3,6 +3,6 @@ import os
 
 os.system('python extractSym.py')
 os.system('python makeTransAndSymTable.py')
+os.system('python phraseSwapping.py')
 os.system('python phraseSegmentation.py')
-#os.system('python makeLM.py')
 os.system('python makeBackOffLM.py')
