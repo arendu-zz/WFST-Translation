d947a96aed8ac0cd513532575b0c64a2e4bb759b
arendu <adithya.r@gmail.com>
2014-03-05 13:01:21 -0500

    cleaning up and added fst2txt

--- fst2txt.py
+++ fst2txt.py
@@ -1,29 +1,21 @@
 __author__ = 'arenduchintala'
-import operator, fst, sys, pdb
 
-
-def bestpathstr(f):
-    fbest = f.shortest_path(1)
-    for i, path in enumerate(fbest.paths()):
-        path_istring = ' '.join(f.isyms.find(arc.ilabel) for arc in path if f.isyms.find(arc.ilabel) != fst.EPSILON)
-        path_ostring = ' '.join(f.osyms.find(arc.olabel) for arc in path if f.osyms.find(arc.olabel) != fst.EPSILON)
-        path_weight = reduce(operator.mul, (arc.weight for arc in path))
-        path_istring = path_istring.encode(encoding='UTF-8', errors='strict')
-        path_ostring = path_ostring.encode(encoding='UTF-8', errors='strict')
-        path_ostring = path_ostring.replace('<s>', '')
-        path_ostring = path_ostring.replace('</s>', '')
-        path_ostring = path_ostring.strip()
-        return path_ostring
-        #paths[float(path_weight)] = paths.get(float(path_weight), []) + [path_ostring + ' <cost=' + str(float(path_weight)) + '>']
-        #return paths
-        #print('{} | {} / {}'.format(path_istring, path_ostring, path_weight))
+import sys, os
 
 
 if __name__ == '__main__':
-    folder = sys.argv[1]
-    if folder[-1] != '/':
-        folder += '/'
-    for idx, s, in enumerate(open('data/input', 'r').readlines()):
-        fname = 'lc' + str(idx) + '.final.fst'
-        f = fst.read(folder + fname)
-        print bestpathstr(f)
+    #symi = fst.read_symbols(sys.argv[2].strip())
+    #symo = fst.read_symbols(sys.argv[3].strip())
+    try:
+        folder = str(sys.argv[1].strip())
+        if folder[-1] != '/':
+            folder += '/'
+        inp_path = str(sys.argv[2].strip())
+    except:
+        print 'useage: python fst2txt.py [folder] [data/input]\n'  # takes 1-best
+        exit()
+    err = []
+    for idx, l in enumerate(open(inp_path, 'r').readlines()):
+        sys.stderr.write(str(idx) + '.' + l)
+        fpath = (folder + 'lc' + str(idx) + '.final.fst')
+        os.system('python printFinal.py ' + fpath)
--- optimize.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-#include <fst/fstlib.h>
-#include <fst/fst-decl.h>
-using namespace fst;
-int main() {
-		cout<<:loading fstfst\n";
-		StdFst *fst = StdFst::Read("full.fst");
-}
